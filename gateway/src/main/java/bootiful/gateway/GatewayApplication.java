7a9cf46033b5d43111c6c4442290cdb61a6c13f9
Josh Long <josh@joshlong.com>
2021-04-07 03:38:10 -0700

    app

--- gateway/src/main/java/bootiful/gateway/GatewayApplication.java
+++ gateway/src/main/java/bootiful/gateway/GatewayApplication.java
@@ -5,6 +5,7 @@ import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -22,36 +23,52 @@ import static org.springframework.web.reactive.function.server.ServerResponse.ok
 @SpringBootApplication
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 
-	private final Sinks.Many<String> updates = Sinks
-		.many()
-		.multicast()
-		.onBackpressureBuffer();
+    private final Sinks.Many<String> updates = Sinks
+            .many()
+            .multicast()
+            .onBackpressureBuffer();
 
-	@Bean
-	Supplier<Flux<String>> updatesSupplier() {
-		return updates::asFlux;
-	}
+    @Bean
+    Supplier<Flux<String>> updatesSupplier() {
+        return updates::asFlux;
+    }
 
-	@SneakyThrows
-	private String buildJsonString(ObjectMapper om, Object object) {
-		return om.writeValueAsString(object);
-	}
+    @SneakyThrows
+    private String buildJsonString(ObjectMapper om, Object object) {
+        return om.writeValueAsString(object);
+    }
 
-	@Bean
-	RouterFunction<ServerResponse> routes(ObjectMapper om) {
-		return route()
-			.GET("/update", request -> {
-				var message = Map.of("message", "Hello, world @ " + Instant.now() + "!");
-				var success = this.updates.tryEmitNext(buildJsonString(om, message));
-				log.info("success: " + success);
-				return ok().bodyValue(message);
-			})
-			.build();
-	}
+    private void doInSpan(Tracer tracer, String spanName) {
+        var newSpan = tracer.nextSpan().name(spanName);
+        try (var spanInScope = tracer.withSpan(newSpan.start())) {
+            newSpan.tag("tag", "a tag");
+        }
+        finally {
+            newSpan.end();
+        }
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> routes(
+            Tracer tracer,
+            ObjectMapper om) {
+
+
+        doInSpan(tracer, "test");
+
+        return route()
+                .GET("/update", request -> {
+                    var message = Map.of("message", "Hello, world @ " + Instant.now() + "!");
+                    var success = this.updates.tryEmitNext(buildJsonString(om, message));
+                    log.info("success: " + success);
+                    return ok().bodyValue(message);
+                })
+                .build();
+    }
 
 
 }
